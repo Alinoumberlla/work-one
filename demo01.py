eba3fb27f7960a442090a942b016651fce3a813b
Alinoumberlla <935079953@qq.com>
2022-02-17 13:36:38 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,55 @@
+# """print("hello world!")# 字符串
+# print(2333)# 整数
+# print(2.333)# 小数
+# print(True)# 布尔值 True False
+# print(())# 元组
+# print([])# 数组
+# print({})# 字典
+# """
+# """
+# 多行注释
+# 多行注释
+
+# print("哈哈",2333,2.333)
+# print("哈哈"+"嘻嘻")# 字符串的拼接(只能相同的类型进行操作)
+# print("哈哈哈"*100)
+# print(((1+2)*100-99)/2)# 用来做数学运算
+# print(((1+2)*100-9.9)/2)    
+# print(2>3)
+# """
+# a = input("请输入:")
+# print("input获取的内容:",a)
+
+# 数据格式的转化 
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# print(type(None))
+# a = int("2333")
+# print(a)
+# a = str("2333")
+# print(type(a))
+# a = str(input("请输入:"))
+# b = str(input("请输入:"))
+# print(a+b)
+# a = float(1.1)
+# b = float(2.21)
+# print(a+b)
+# a = "ikjiijiuio huiohaduihuiohad io ioadhoi"
+# print(len(a)) #len()获取字符串长度
+# a = "dhnihidhauihuiui hdiawuhidaiohdaowdih"
+# b = "wdbnujbhauidbhuiawbgdiuagbrfiugbaui hwuiahgruigha iwurgaiurgui"
+# print(len(a)+len(b)) #通过代码获取两段内容，并且计算他们两段长度之和
+a = len(input("请输入:"))
+b = len(input("请输入:"))
+print(a+b)
+
+
+
+
+
