20685075a8aeb2411922465c0b7bd9374a6dca78
Alinoumberlla <935079953@qq.com>
2022-02-22 18:27:06 +0800

    练习

--- demo01.py
+++ demo01.py
@@ -193,7 +193,7 @@
 # c = ["张三","李四","王麻子","浪静","流云","嘻嘻","小梁","二狗","成平安","猪猪","亚索"]
 # x = 0
 # while x < 11:
-#     score = int(input("请输入成绩:"))
+#     score = int(input("请输入"+c[x]+"的成绩:"))
 #     if score >= 60:
 #         a[c[x]] = score
 #     else:
@@ -204,26 +204,104 @@
 #想了一个小时的结果本来是只能循环不断更改a,b字典里面的值，不能新增。后来照着代码对着修改最终的结果。
 #下面是老师写的代码
 
-highscore = {}
-lowscore = {}
-studentlist = ["张三","李四","王麻子","浪静","流云","嘻嘻","小梁","二狗","成平安","猪猪","亚索"]
-a = 0
-while a < len(studentlist):
-    score = int(input("请输入"+studentlist[a]+"的成绩:"))
-    if score >=60:
-        highscore[studentlist[a]] = score
-    else:
-        lowscore[studentlist[a]] = score
-    a = a + 1
-print("大于60的:",highscore)
-print("小于60的:",lowscore)
+# highscore = {}
+# lowscore = {}
+# studentlist = ["张三","李四","王麻子","浪静","流云","嘻嘻","小梁","二狗","成平安","猪猪","亚索"]
+# a = 0
+# while a < len(studentlist):
+#     score = int(input("请输入"+studentlist[a]+"的成绩:"))
+#     if score >=60:
+#         highscore[studentlist[a]] = score
+#     else:
+#         lowscore[studentlist[a]] = score
+#     a = a + 1
+# print("大于60的:",highscore)
+# print("小于60的:",lowscore)
 
+# highscore = {}
+# lowscore = {}
+# studentlist = ["张三","李四","王麻子","浪静","流云","嘻嘻","小梁","二狗","成平安","猪猪","亚索"]
+# for i in studentlist:
+#     score = int(input("请输入"+i+"的成绩:"))
+#     if score >=60:
+#         highscore[i] = score
+#     else:
+#         lowscore[i] = score
+# print("大于60的:",highscore)
+# print("小于60的:",lowscore)
 
+# for循环  (遍历)
+# a = ["张三","李四","王麻子","浪静","流云","嘻嘻","小梁","二狗","成平安","猪猪","亚索"]
+# for i in a:
+#     print(i)
 
+# b = list(range(0,100)) #左闭右开
+# print(b)
    
+# b = range(50,60,2)
+# for i in b:
+#     print(i)
+
+#练习 打印一个九九乘法表
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(i,"x",j,"=",i*j,end="   ")
+#     print()
+        
+#练习 通过print打印，模拟一个红绿灯的功能，注意：红灯30次，绿灯35次，黄灯3次。
+# a = range(30,0,-1)
+# b = range(35,0,-1)
+# c = range(3,0,-1)
+# x = 0
+# while x == 0:
+#     for i in a:
+#         print("红灯还有"+str(i)+"秒")
+#     for j in b:
+#         print("绿灯还有"+str(j)+"秒")
+#     for k in c:
+#         print("黄灯还有"+str(k)+"秒")
+
+# light = {"红灯":30,"绿灯":35,"黄灯":3}
+# while True:
+#     for i in light:
+#         for j in range(light[i]):
+#             print(i+"还有"+str(light[i]-j)+"秒")
 
 
 
+#练习 使用代码实现一个注册的功能。用户输入账号和密码，要求账号长度是5-8位，密码6-12位，并且账号必须小写开头。
+#     储存到字典中，{username ： password}
+
+# x = 0
+# while x == 0:
+#     username = input("请输入账号:")
+#     if len(username) < 5 or len(username) > 8:
+#             print("账号长度为5-8位,请重新输入")
+#     else:
+#         print("账号可用")
+#         y = 0 
+#         while y == 0:
+#                 password = input("请输入密码:")
+#                 if len(password) < 6 or len(password) > 12:
+#                     print("密码的长度为6-12位,请重新输入")
+#                 else:
+#                     print("密码可用")   
+#                     print({username:password})
+#                     exit()
+
+# username = input("请输入账号:")
+# password = input("请输入密码:")
+# if len(username) >=5 and len(username) <=8:
+#     if username[0] in "abcdefghijklmnopqrstuvwxyz":
+#         if len(password)>=6 and len(password)<=12:
+#             print("密码可用")
+#             print({username:password})
+#         else:
+#             print("密码的长度为6-12位,请重新输入")
+#     else:
+#         print("账号首字母必须小写")    
+# else:
+#     print("账号长度为5-8位,请重新输入")
 
 
 
